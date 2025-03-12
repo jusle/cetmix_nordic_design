cbb739eb3d12142431f39545418123757b123171
Jussi Lehto <jussi.lehto@odoo-bs.fi>
2025-03-12 19:18:27 +0200

    add dosis font module

---
+++ cetmix_dosis_font/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2024 Ab Cetmix Nordic Oy
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Dosis Font",
+    "summary": "Dosis font for reports",
+    "version": "saas~17.2.1.0.0",
+    "assets": {
+        "web.report_assets_common": [
+            "/cetmix_dosis_font/static/src/scss/dosis.scss",
+        ]
+    }
+}
