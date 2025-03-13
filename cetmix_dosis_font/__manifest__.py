ab14dea49b3e39a75a0494be8fe392e5af819258
Jussi Lehto <jussi.lehto@odoo-bs.fi>
2025-03-13 03:21:26 +0200

    version to 18.0

--- cetmix_dosis_font/__manifest__.py
+++ cetmix_dosis_font/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2024 Ab Cetmix Nordic Oy
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
-    "name": "Dosis Font",
-    "summary": "Dosis font for reports",
-    "version": "18.0.1.0.0",
-    "assets": {
-        "web.report_assets_common": [
-            "/cetmix_dosis_font/static/src/scss/dosis.scss",
+    'name': 'Dosis Font',
+    'summary': 'Dosis font for reports',
+    'version': '18.0.1.0.0',
+    'assets': {
+        'web.report_assets_common': [
+            'cetmix_dosis_font/static/src/scss/dosis.scss',
         ]
     }
 }
