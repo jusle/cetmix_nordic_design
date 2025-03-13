c35b7bfc939a2ed54e7d00593cbaea2f04afe78e
Jussi Lehto <jussi.lehto@odoo-bs.fi>
2025-03-13 10:01:09 +0200

    updated for 18

--- cetmix_dosis_font/__manifest__.py
+++ cetmix_dosis_font/__manifest__.py
@@ -1,12 +1,29 @@
 # Copyright 2024 Ab Cetmix Nordic Oy
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
-    'name': 'Dosis Font',
-    'summary': 'Dosis font for reports',
-    'version': '18.0.1.0.0',
+    'name': 'Cetmix Dosis Font',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Custom Dosis Font for Odoo',
+    'description': """
+        This module adds the Dosis font to Odoo.
+    """,
+    'author': 'Cetmix Nordic Ltd.',
+    'website': 'http://www.cetmix.fi',
+    'depends': ['base'],
+    ],
     'assets': {
-        'web.report_assets_common': [
-            'cetmix_dosis_font/static/src/scss/dosis.scss',
-        ]
-    }
+        'web.assets_frontend': [
+            'cetmix_dosis_font/static/src/css/dosis.css',
+        ],
+        'web.assets_backend': [
+            'cetmix_dosis_font/static/src/css/dosis.css',
+        ],
+        'web.assets_common': [
+            'cetmix_dosis_font/static/src/css/dosis.css',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
